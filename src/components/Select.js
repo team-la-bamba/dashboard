aea2c5fd2f7ac81cc495fcc0965dd374f0d71c04
Eleonor Dammfors <eleonor.dammfors@gmail.com>
2020-04-05 14:51:39 +0200

    add Header.js and border

--- src/components/Select.js
+++ src/components/Select.js
@@ -2,33 +2,36 @@ import React from 'react';
 import Select from 'react-select';
 
 export default ({ label, onChange, options, placeholder, value }) => {
-  const selected = options.filter((o) => o.value === value).pop();
+  const selected = options.filter(o => o.value === value).pop();
 
   const customStyles = {
-    option: (provided) => ({
+    option: provided => ({
       ...provided,
       padding: 20,
-      color: '#333',
+      color: '#333'
     }),
-    control: (provided) => ({
+    control: provided => ({
       ...provided,
       boxShadow: '0 1px 3px 0 rgba(0,0,0,.1), 0 1px 2px 0 rgba(0,0,0,.06)',
       borderColor: '#9fa6b2',
-      padding: '0',
+      padding: '0'
     }),
-    valueContainer: (provided) => ({
+    valueContainer: provided => ({
       ...provided,
-      padding: '0',
+      padding: '0'
     }),
     indicatorSeparator: () => ({}),
-    dropdownIndicator: (provided) => ({
+    dropdownIndicator: provided => ({
       ...provided,
-      color: '#333',
+      color: '#333'
     }),
     placeholder: () => ({
       color: '#333',
-      paddingLeft: '1rem',
+      paddingLeft: '1rem'
     }),
+    singleValue: () => ({
+      paddingLeft: '1rem'
+    })
   };
 
   return (
@@ -36,7 +39,7 @@ export default ({ label, onChange, options, placeholder, value }) => {
       aria-label={label}
       placeholder={placeholder}
       value={selected}
-      onChange={(selected) => onChange(selected ? selected.value : null)}
+      onChange={selected => onChange(selected ? selected.value : null)}
       isSearchable={true}
       isClearable={true}
       options={options}
--- src/components/layout/Dashboard.js
+++ src/components/layout/Dashboard.js
@@ -1,14 +1,14 @@
 import React, { useState, useEffect } from 'react';
 
 import Sidebar from './Sidebar';
-
+import Header from '../Header';
 import LineChart from '../LineChart';
 import Stats from '../Stats';
 import TableChart from '../TableChart';
 
 const url = 'https://api.labamba.space';
 
-const serialize = (obj) => {
+const serialize = obj => {
   let str = [];
   for (const p in obj) {
     if (obj.hasOwnProperty(p)) {
@@ -22,11 +22,11 @@ const Layout = () => {
   const [answers, setAnswers] = useState([]);
   const [loading, setLoading] = useState(false);
 
-  const fetchAnswers = (values) => {
+  const fetchAnswers = values => {
     setLoading(true);
     fetch(url + '/answers?' + serialize(values))
-      .then((res) => res.json())
-      .then((body) => {
+      .then(res => res.json())
+      .then(body => {
         setLoading(false);
         setAnswers(body);
       });
@@ -37,36 +37,40 @@ const Layout = () => {
   }, []);
 
   return (
-    <div className="h-screen flex bg-gray-100">
-      <Sidebar onClick={(values) => fetchAnswers(values)} />
+    <div>
+      <div className="h-screen flex bg-gray-100">
+        <Sidebar onClick={values => fetchAnswers(values)} />
 
-      <div className="flex flex-col w-0 flex-1 overflow-hidden">
-        <main
-          className="flex-1 relative z-0 overflow-y-auto pt-2 pb-6 focus:outline-none md:py-6"
-          tabIndex="0"
-        >
-          {loading ? (
-            <p className="text-center">Loading...</p>
-          ) : (
-            <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
-              <div className="py-4">
-                <Stats />
+        <div className="flex flex-col w-0 flex-1 overflow-hidden">
+          <Header />
 
-                <div className="mt-10 grid grid-flow-row grid-cols-2 gap-4">
-                  {answers.map((q, qi) => (
-                    <div key={qi}>
-                      <TableChart {...q} />
-                    </div>
-                  ))}
-                </div>
+          <main
+            className="flex-1 relative z-0 overflow-y-auto pt-2 pb-6 focus:outline-none md:py-6"
+            tabIndex="0"
+          >
+            {loading ? (
+              <p className="text-center">Loading...</p>
+            ) : (
+              <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
+                <div className="py-4">
+                  <Stats />
 
-                <div className="mt-10">
-                  <LineChart />
+                  <div className="mt-10 grid grid-flow-row grid-cols-2 gap-4">
+                    {answers.map((q, qi) => (
+                      <div key={qi}>
+                        <TableChart {...q} />
+                      </div>
+                    ))}
+                  </div>
+
+                  <div className="mt-10">
+                    <LineChart />
+                  </div>
                 </div>
               </div>
-            </div>
-          )}
-        </main>
+            )}
+          </main>
+        </div>
       </div>
     </div>
   );
